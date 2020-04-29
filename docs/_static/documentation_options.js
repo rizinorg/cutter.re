e21ed16db05cd1c02265248e22b7b746b0e2e0f6
Travis CI <travis@travis-ci.com>
2020-04-29 20:26:20 +0000

    Update docs from radareorg/cutter
    Original Commit:
    a3661fa2595004bf255726a02a99e5141415bc9b Update version to 1.10.3-pre2 . (#2171)

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.10.3-pre',
+    VERSION: '1.10.3-pre2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
