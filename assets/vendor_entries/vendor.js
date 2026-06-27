371bdd08bd9d29d89821e83d2b8b237bcb41d3ab
ITAYC0HEN <ITAYC0HEN@users.noreply.github.com>
2026-06-27 10:58:14 +0000

    deploy: 0ddbcf151b808c62295d4e32c5d8338b23e6d82e

---
+++ assets/vendor_entries/vendor.js
@@ -0,0 +1,15 @@
+// All imports in this file will be compiled into vendors.js file.
+//
+// Note: ES6 support for these imports is not supported in base build
+
+module.exports = [
+  './node_modules/jquery/dist/jquery.js',
+  './node_modules/bootstrap/dist/js/bootstrap.min.js',
+  './src/js/modules/jquery.easing.min.js',
+  './src/js/modules/slimmenu.min.js',
+  './src/js/modules/aos.min.js',
+  './src/js/modules/highlight.pack.js',
+  './src/js/modules/owl.carousel.min.js',
+  './src/js/modules/swipper.min.js',
+  './src/js/modules/owl.carousel.thumbs.min.js',
+];
